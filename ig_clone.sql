6d7ed53312ac085102b00fcc208ca735f31c1e05
Rocky Singh <rockygsingh3@gmail.com>
2018-03-02 06:38:47 +0000

    crated user table

---
+++ ig_clone.sql
@@ -0,0 +1,9 @@
+-- INSTAGRAM TABLES 
+
+CREATE TABLE user (
+    id INT AUTO_INCRMENT PRIMARY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    user_name VARCHAR(100),
+    email VARCHAR(100)
+);
