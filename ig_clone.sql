b505aaf2f9d9dff0ca9aae53f78e77fd03cf4a71
Rocky Singh <rockygsingh3@gmail.com>
2018-03-02 07:27:53 +0000

    created follows tables

--- ig_clone.sql
+++ ig_clone.sql
@@ -38,3 +38,13 @@ CREATE TABLE likes (
     PRIMARY KEY(user_id, photo_id)
 );
 
+
+CREATE TABLE follows (
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
