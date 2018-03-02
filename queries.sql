ded36c5248d619f89a586a60f452cdc498850a2c
Rocky Singh <rockygsingh3@gmail.com>
2018-03-02 15:16:30 +0000

    What day of the week do most users register on

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
 
 
 -- find the 5 oldest users (Maybe I want to reward them)
-SELECT username, created_at AS time  FROM users ORDER BY created_at LIMIT 5; 
+SELECT username, created_at AS time of account crated  FROM users ORDER BY created_at LIMIT 5; 
