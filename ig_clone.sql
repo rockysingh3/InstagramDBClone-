43a6cc9e2d60f5d475cb316a5f1bfee05846ce80
Rocky Singh <rockygsingh3@gmail.com>
2018-03-02 06:47:46 +0000

    added photos schema

--- ig_clone.sql
+++ ig_clone.sql
@@ -9,3 +9,11 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE photos (
+    id INT AUTO_INCRMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id);
+);
+
