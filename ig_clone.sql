8ae699c0f1df5eb908fd5c826fd37931be1f8717
Rocky Singh <rockygsingh3@gmail.com>
2018-03-02 06:55:45 +0000

    Added comments table

--- ig_clone.sql
+++ ig_clone.sql
@@ -17,3 +17,14 @@ CREATE TABLE photos (
     FOREIGN KEY(user_id) REFERENCES users(id);
 );
 
+
+CREATE TABLE comments (
+    id INT AUTO_INCRMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
