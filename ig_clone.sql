c2f18c63a407df25cfdb534b6b3b1d1ba3c462fc
Rocky Singh <rockygsingh3@gmail.com>
2018-03-02 09:55:43 +0000

    crated tags and photo_tags table

--- ig_clone.sql
+++ ig_clone.sql
@@ -48,3 +48,18 @@ CREATE TABLE follows (
     PRIMARY KEY(follower_id, followee_id)
 );
 
+
+CREATE TABLE tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+    photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
+
