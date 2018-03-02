a4842ae53239d0ccc944fec1c0c85e6699ddcd6e
Rocky Singh <rockygsingh3@gmail.com>
2018-03-02 07:20:34 +0000

    added likes table

--- ig_clone.sql
+++ ig_clone.sql
@@ -1,7 +1,7 @@
 -- INSTAGRAM TABLES 
 
 CREATE TABLE users (
-    id INTEGER AUTO_INCRMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     user_name VARCHAR(100) UNIQUE NOT NULL,
@@ -10,21 +10,31 @@ CREATE TABLE users (
 );
 
 CREATE TABLE photos (
-    id INT AUTO_INCRMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     image_url VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(user_id) REFERENCES users(id);
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 
 CREATE TABLE comments (
-    id INT AUTO_INCRMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     comment_text VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
     photo_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+
+CREATE TABLE likes (
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
 );
 
